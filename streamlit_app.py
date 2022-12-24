08737d503b67aa0f0e9d215e21db0e764017074d
lilwilla <kingaengi@gmail.com>
2022-12-24 18:31:09 +0100

    Update streamlit_app.py
    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 
 import streamlit
 
-streamlit.title('Diner')
+streamlit.title('My Parents Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
